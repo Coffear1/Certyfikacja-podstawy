2f4d17c2e4bef2f417489687c3d3f7712bcb037a
Krzysiek <krzysiek.stebel@gmail.com>
2024-04-16 17:50:42 +0200

    Add FootballerInMemory implementation

---
+++ Certyfikacja podstawy/Certyfikacja podstawy/FootballerBase.cs
@@ -0,0 +1,47 @@
+﻿namespace Certyfikacja_podstawy
+{
+    public abstract class FootballerBase : IFootballer
+    {
+        public delegate void ScoreAddedDelegate(object sender, EventArgs args);
+
+        public abstract event ScoreAddedDelegate ScoreAdded;
+        public FootballerBase(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+       
+        public string Name { get;}
+        public string Surname { get;}
+        public int Age { get;}
+
+        public abstract void AddScore(float score);
+        
+        public void AddScore(int score)
+        {
+           float IntAsFloat = (float)score;
+           this.AddScore(IntAsFloat);
+        }
+
+        public void AddScore(string score)
+        {
+            if (float.TryParse(score, out float result))
+            {
+                this.AddScore((float)result);
+            }
+            else
+            {
+                throw new Exception("String nie jest floatem");
+            }
+        }
+
+        public void AddScore(char score)
+        {
+            float CharAsFloat = (float)score;
+            this.AddScore(CharAsFloat);
+        }
+
+        public abstract Statistics GetStatistics();
+    }
+}
---
+++ Certyfikacja podstawy/Certyfikacja podstawy/FootballerInMemory.cs
@@ -0,0 +1,39 @@
+﻿namespace Certyfikacja_podstawy
+{
+    public class FootballerInMemory : FootballerBase
+    {
+        public override event ScoreAddedDelegate ScoreAdded;
+
+        private List<float> scores = new List<float>();
+
+        public FootballerInMemory(string name, string surname, int Age)
+           : base(name, surname, Age)
+        {
+        }
+        public override void AddScore(float score)
+        {
+            if (score >= 0 && score <= 100)
+            {
+                this.scores.Add(score);
+            }
+            else
+            {
+                throw new Exception("Nie można dodać tej oceny. Proszę używać cyfr i liczb z zakresu 0-100");
+            }
+            if (score >= 3 && score <= 100 && ScoreAdded != null)
+            {
+                ScoreAdded(this, new EventArgs());
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+            foreach (var score in this.scores)
+            {
+                statistics.AddScore(score);
+            }
+            return statistics;
+        }
+    }
+}
